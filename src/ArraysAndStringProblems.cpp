368dd98b46c407e4c56079a8498a5367d846cb56
Swarnendu Roy Chowdhury <112170550+Swarnendu-R-C@users.noreply.github.com>
2024-03-23 22:06:04 +0530

    C++ and python solutions for 3rd array algo problem added

--- src/ArraysAndStringProblems.cpp
+++ src/ArraysAndStringProblems.cpp
@@ -1,3 +1,13 @@
+/**
+ * @file ArraysAndStringProblems.cpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-03-22
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
 #include "ArraysAndStringProblems.hpp"
 
 #include <regex>
@@ -135,4 +145,117 @@ bool AlgoProblems::checkPermutation(std::string stringOne,
         return true;
     };
     return solution(stringOne, stringTwo) && solutionOpt();
+}
+
+bool AlgoProblems::palindromePermutation(std::string theString)
+{
+    if (theString.empty())
+        return false;
+
+    /**
+     * What does it take to be able to write a set of characters the same way forwards and backwards? We need to
+     * have an even number of almost all characters, so that half can be on one side and half can be on the other
+     * side. At most one character (the middle character) can have an odd count.
+     * 
+     * To be more precise, strings with even length (after removing ail non-letter characters) must have
+     * all even counts of characters. Strings of an odd length must have exactly one character with
+     * an odd count. Of course, an "even" string can't have an odd number of exactly one character,
+     * otherwise it wouldn't be an even-length string (an odd number + many even numbers = an odd
+     * number). Likewise, a string with odd length can't have all characters with even counts (sum of
+     * evens is even). It's therefore sufficient to say that, to be a permutation of a palindrome, a string
+     * can have no more than one character that is odd. This will cover both the odd and the even cases.
+     */
+    // First of all remove any whitespcaes for simplicity of operations
+    std::regex ws("\\s");
+    theString = std::regex_replace(theString, ws, "");
+
+    // Convert all the letters either in upper case or lower case for more simplicity
+    for (auto& letter : theString)
+        letter = std::tolower(letter);
+
+    /**
+     * @brief So the first solution is to make a hash table of characters
+     * avilable in the string with each of their count and find if there is
+     * any characater having an odd count more than once. O(N) time
+     */
+    auto firstSolution = [theString]()
+    {
+        std::unordered_map<char, int> charMap;
+        for (const auto& charecter : theString)
+        {
+            auto itr = charMap.find(charecter);
+            if (itr != charMap.end())
+                itr->second++;
+            else
+                charMap[charecter] = 1;
+        }
+        auto oddCnt = 0;
+        for (const auto& itr : charMap)
+        {
+            if (itr.second % 2 > 0)
+                oddCnt++;
+        }
+        return oddCnt <= 1;
+    };
+    /**
+     * @brief Second solution is also of O(N) but slight optimization
+     * over the first. First solution was actually of O(2N) here it
+     * would be only O(N). P.S. Not counting the sorting and converting
+     * to upper/lower case as those are common to all        
+     */
+    auto secondSolution = [theString]()
+    {
+        std::unordered_map<char, int> charMap;
+        auto oddCnt = 0;
+        for (const auto& charecter : theString)
+        {
+            auto itr = charMap.find(charecter);
+            if (itr != charMap.end())
+                itr->second++;
+            else
+                charMap[charecter] = 1;
+
+            if (charMap[charecter] %2 > 0)
+                ++oddCnt;
+            else
+                --oddCnt;
+        }
+        return oddCnt <= 1;
+    };
+    /**
+     * @brief If you think more deeply about this problem, you might notice that we don't actually need to know the
+     * counts. We just need to know if the count is even or odd. Think about flipping a light on/off (that is initially
+     * off). If the light winds up in the o f f state, we don't know how many times we flipped it, but we do know it
+     * was an even count,
+     * Given this, we can use a single integer (as a bit vector). When we see a letter, we map it to an integer
+     * between 0 and 26 (assuming an English alphabet). Then we toggle the bit at that value. At the end of the
+     * iteration, we check that at most one bit in the integer is set to 1.
+     * 
+     * We can easily check that no bits in the integer are 1: just compare the integer to 0. There is actually a very
+     * elegant way to check that an integer has exactly one bit set to 1.
+     * 
+     * Picture an integer like 00010000. We could of course shift the integer repeatedly to check that there's only
+     * a single 1. Alternatively, if we subtract 1 from the number, we'll get 00001111. What's notable about this
+     * is that there is no overlap between the numbers (as opposed to say 00101000, which, when we subtract 1
+     * from, we get 00100111.) So, we can check to see that a number has exactly one 1 because if we subtract 1
+     * from it and then AND it with the new number, we should get 0.
+     * 
+     * 00010000 - 1 = 00001111
+     * 00010000 & 00001111 = 0
+     */
+    auto thirdSolution = [theString]()
+    {
+        std::bitset<26> alphabets;
+        char smallA = 'a';
+        for (const auto& alpha : theString)
+        {
+            auto idx = static_cast<int>(alpha - smallA);
+            alphabets[idx].flip();
+        }
+        return !(alphabets.count() > 1);
+    };
+    //return firstSolution();
+    //return secondSolution();
+    //return thirdSolution();
+    return firstSolution() && secondSolution() && thirdSolution();
 }
--- src/TestSolutionsArraysAndStrings.cpp
+++ src/TestSolutionsArraysAndStrings.cpp
@@ -1,3 +1,14 @@
+/**
+ * @file TestSolutionsArraysAndStrings.cpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-03-22
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
 #include "ArraysAndStringProblems.hpp"
 
 TEST_F(AlgoProblems, testIsUnique)
@@ -50,5 +61,62 @@ TEST_F(AlgoProblems, testPermutation)
         EXPECT_TRUE(checkPermutation(data1, data2, true));
         EXPECT_FALSE(checkPermutation(data1, data2, true, false));
     }
+}
+
+TEST_F(AlgoProblems, testPalindromePermutation)
+{
+    {
+        std::vector<std::string> palindromes = { "radar", "level", "deified", "madam", "racecar", "noon", "civic", "rotor", "refer", "redder" };
+        std::vector<std::string> palindormesPermutations;        
+        for (auto item : palindromes)
+        {
+            std::sort(item.begin(), item.end());
+            do
+            {
+                palindormesPermutations.emplace_back(item);
+            } while (std::next_permutation(item.begin(), item.end()));            
+        }
+        for (const auto& item : palindormesPermutations)
+            EXPECT_TRUE(palindromePermutation(item));
+    }
+    {
+        std::vector<std::string> palindromes = { "radara", "wlevel", "edeified", "nmadam", "aacecar", "pmnoon", "mcivic", "mrotor", "trefer" };
+        std::vector<std::string> palindormesPermutations;        
+        for (auto item : palindromes)
+        {
+            std::sort(item.begin(), item.end());
+            do
+            {
+                palindormesPermutations.emplace_back(item);
+            } while (std::next_permutation(item.begin(), item.end()));            
+        }
+        for (const auto& item : palindormesPermutations)
+            EXPECT_FALSE(palindromePermutation(item)) << "string = " << item;
+    }
+    // Commented as it takes super long to generate all the permutations
+    /* {
+        std::vector<std::string> palindromes = 
+            { "a man a plan a canal panama"
+                , "was it a car or a cat i saw"
+                , "evil is a name of a foeman as i live"
+                , "never odd or even"
+                , "able was i saw elba"
+                , "do geese see god"
+                , "mr owl ate my metal worm"
+                , "a Santa lived as a devil at NASA"
+                , "step on no pets"
+            };
+        std::vector<std::string> palindormesPermutations;        
+        for (auto item : palindromes)
+        {
+            std::sort(item.begin(), item.end());
+            do
+            {
+                palindormesPermutations.emplace_back(item);
+            } while (std::next_permutation(item.begin(), item.end()));            
+        }
+        for (const auto& item : palindormesPermutations)
+            EXPECT_TRUE(palindromePermutation(item));
+    } */
 }
 
